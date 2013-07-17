c415089c99a4c7251799bb7295ef5d1fbb7be896
KILLHAPPY. <graybobo@live.cn>
2013-07-17 19:59:24 +0800

    Update Testing script

--- Testing.js
+++ Testing.js
@@ -6,13 +6,15 @@
 require("./Date.strftime");
 
 var TestingDate = new Date();
-// Shorthand format one [ like 2013-05-03 ]
+// Shorthand format one: like [ 2013-05-03 ]
 console.log( 'Format one: ' + TestingDate.strftime( Date.Formats.O ) );
-// Shorthand format two [ like 05/03/13 ]
+// Shorthand format two: like [ 05/03/13 ]
 console.log( 'Format two: ' + TestingDate.strftime( Date.Formats.T ) );
-// Shorthand format three [ like 2013年05月03日 ]
+// Shorthand format three: like [ 2013年05月03日 ]
 console.log( 'Format three: ' + TestingDate.strftime( Date.Formats.C ) );
-// Shorthand format four [ like 2013-05-03 22:56:23 ]
+// Shorthand format four: like [ 2013-05-03 22:56:23 ]
 console.log( 'Format four: ' + TestingDate.strftime( Date.Formats.F ) );
-// Shorthand format five [ like 2013-05-04 星期六 ]
+// Shorthand format five: like [ 2013-05-04 星期六 ]
 console.log( 'Format five: ' + TestingDate.strftime( Date.Formats.W ) );
+// Shorthand format six: like [ 2013-07-17 Wednesday ]
+console.log( 'Format five: ' + TestingDate.strftime( Date.Formats.E ) );
